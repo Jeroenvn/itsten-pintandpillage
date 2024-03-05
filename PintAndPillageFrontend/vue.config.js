878c3cd9dee8e40e3160447ea5fe20a271661727
Jeroen <s1137642@student.hsleiden.nl>
2024-03-05 17:25:14 +0100

    fix: requests went to localhost instead of the proper hostname

--- PintAndPillageFrontend/vue.config.js
+++ PintAndPillageFrontend/vue.config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://backend:8080',
         secure: true,
       }
     },
